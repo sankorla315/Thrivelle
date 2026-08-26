180abdaf25d4a9d403a9390eeee9fd4e6b1e3c37
sankorla315 <sankeerna@simplepractice.com>
2026-08-25 20:35:30 -0500

    fix(lint): configure eslint setup

--- tooling/eslint-config/base.js
+++ tooling/eslint-config/base.js
@@ -1,1 +1,18 @@
-export default [{ ignores: ["dist/*", ".next/", ".turbo/*"] }];
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", ".next/**", ".turbo/**", "coverage/**", "node_modules/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" },
+      ],
+    },
+  },
+);
